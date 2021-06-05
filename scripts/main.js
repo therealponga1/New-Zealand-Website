6864459ea773695ad820613d29fdd4b4e4abd169
Luke Devops <developmentdesignsltd@gmail.com>
2021-06-06 07:55:42 +1200

    Updated All files

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function validateForm() {
+    var x = document.forms["myForm"]["name"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      
+      return false;
+    }else{
+      return alert("Contacted Successfully");
+    }
+  };
